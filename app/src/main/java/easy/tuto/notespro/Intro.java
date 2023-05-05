a625f7e5cd8870afb10f950c7abd0ae11814dd1b
Hamza Laabidi <99836375+laabidihamza@users.noreply.github.com>
2023-05-06 00:39:46 +0100

    im done

---
+++ app/src/main/java/easy/tuto/notespro/Intro.java
@@ -0,0 +1,14 @@
+package easy.tuto.notespro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Intro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_pen_pal);
+    }
+}
